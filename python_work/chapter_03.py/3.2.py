dd7d2d18b97234ee4f111f26637a4f9a7852df63
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-08-20 22:23:13 -0300

    Create 3.2.py

---
+++ python_work/chapter_03.py/3.2.py
@@ -0,0 +1,5 @@
+names = ['livia', 'maria', 'ricardo', 'murilo']
+print(f'Olá, {names[0].title()}, bom dia!')
+print(f'Olá, {names[1].title()}, bom dia!')
+print(f'Olá, {names[2].title()}, bom dia!')
+print(f'Olá, {names[3].title()}, bom dia!')
