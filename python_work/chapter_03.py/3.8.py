a30bc28c33716adceff8db28bb11576874e4798e
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-08-20 22:23:30 -0300

    Create 3.8.py

---
+++ python_work/chapter_03.py/3.8.py
@@ -0,0 +1,18 @@
+lugares_mundo = ['Itália', 'Tailândia', 'Amsterdam', 'Dubai', 'Holanda']
+
+print(sorted(lugares_mundo))
+print(lugares_mundo)
+print(sorted(lugares_mundo, reverse=True))
+print(lugares_mundo)
+
+lugares_mundo.reverse()
+print(lugares_mundo)
+
+lugares_mundo.reverse()
+print(lugares_mundo)
+
+lugares_mundo.sort()
+print(lugares_mundo)
+
+lugares_mundo.sort(reverse=True)
+print(lugares_mundo)
