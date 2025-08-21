1f09bd17d3bf55222675fdf47e66acee8b89dac8
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-08-20 22:23:38 -0300

    Create 3.11.py

---
+++ python_work/chapter_03.py/3.11.py
@@ -0,0 +1,7 @@
+# Erro intencional
+convidados = ['Orlando', 'Célia', 'Ricardo', 'Leoni', 'Livia']
+
+convidados.remove('Livia')
+# print(convidados[4]) # IndexError: list index out of range
+print(convidados[3])
+
