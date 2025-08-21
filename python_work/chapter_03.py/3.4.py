adfa2380e9044b26b235ad336442f6c83a35b046
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-08-20 22:23:21 -0300

    Create 3.4.py

---
+++ python_work/chapter_03.py/3.4.py
@@ -0,0 +1,4 @@
+convidados = ['Orlando', 'Célia', 'Ricardo']
+print(f'Olá, {convidados[0]}! Gostaria de te convidar para um jantar!')
+print(f'Olá, {convidados[1]}! Gostaria de te convidar para um jantar!')
+print(f'Olá, {convidados[2]}! Gostaria de te convidar para um jantar!')
