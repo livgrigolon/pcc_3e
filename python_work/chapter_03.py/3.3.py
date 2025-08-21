91252622ce9e033815ad64f81c55982b0f2c8de1
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-08-20 22:23:16 -0300

    Create 3.3.py

---
+++ python_work/chapter_03.py/3.3.py
@@ -0,0 +1,4 @@
+carros = ['Porsche', 'BMW', 'Ferrari']
+print(f'Gostaria de ter um carro da {carros[0]}')
+print(f'Gostaria de ter um carro da {carros[1]}')
+print(f'Gostaria de ter um carro da {carros[2]}')
