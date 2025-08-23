1a509ae5bbe6e2e0e77cb06a71595a0b9dcb8b35
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-08-22 23:27:56 -0300

    Create 4.2.py

---
+++ python_work/chapter_04/4.2.py
@@ -0,0 +1,6 @@
+animais = ['Calopsita', 'Papagaio', 'Maritaca']
+
+for animal in animais:
+    print(f'Um(a) {animal.lower()} seria um ótimo animal de estimação.')
+
+print('Qualquer um desses animais podem voar!')
