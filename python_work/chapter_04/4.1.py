df3df7f495e486b855afbcfcb691567f15bdbb32
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-08-22 23:27:53 -0300

    Create 4.1.py

---
+++ python_work/chapter_04/4.1.py
@@ -0,0 +1,7 @@
+pizzas = ['Pepperoni', 'Bacon', 'Mussarela', 'Queijos']
+
+for pizza in pizzas:
+    print(f'Gosto de pizza de {pizza}')
+
+print(f'Minha pizza favorita é de {pizzas[0]} \nMinha segunda pizza favorita é de {pizzas[1]} \nTambém gosto da pizza de {pizzas[2]}')
+print('Eu amo pizzas!')
