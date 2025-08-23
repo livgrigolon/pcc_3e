4fbb9a9345128a79bc6dfe3c37d5248bd4c64777
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-08-22 23:28:02 -0300

    Create 4.5.py

---
+++ python_work/chapter_04/4.5.py
@@ -0,0 +1,8 @@
+milhao = list(range(1, 1000001))
+
+for numero in milhao:
+    print(numero)
+
+print(min(milhao))   # menor valor
+print(max(milhao))   # maior valor
+print(sum(milhao))   # soma de todos os valores
