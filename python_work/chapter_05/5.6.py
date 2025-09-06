023976522279bd089575370a8acc218a14a351d4
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-09-06 19:37:17 -0300

    Create 5.6.py

---
+++ python_work/chapter_05/5.6.py
@@ -0,0 +1,14 @@
+age = 65
+
+if age < 2:
+    print('A pessoa é um neném')
+elif age >= 2 and age < 4:
+    print('A pessoa é uma criança')
+elif age >= 4 and age < 13:
+    print('A pessoa é um(a) garoto(a)')
+elif age >= 13 and age < 20:
+    print('A pessoa é um adolescente')
+elif age >= 20 and age < 65:
+    print('A pessoa é um adulto')
+else:
+    print('A pessoa é um idoso')
