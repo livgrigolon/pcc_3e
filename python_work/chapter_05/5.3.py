1bc06567fa58c9ab37f41ffefe98b62778089c6a
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-09-06 19:37:11 -0300

    Create 5.3.py

---
+++ python_work/chapter_05/5.3.py
@@ -0,0 +1,10 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('Você acabou de ganhar 5 pontos!')
+
+# Versão que falha
+# alien_color = 'red'
+
+# if alien_color == 'green':
+#     print('Você acabou de ganhar 5 pontos!')
