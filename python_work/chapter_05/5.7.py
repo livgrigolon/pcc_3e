377cef26c4ae52001f79adf9ff86a71d043274df
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-09-06 19:37:22 -0300

    Create 5.7.py

---
+++ python_work/chapter_05/5.7.py
@@ -0,0 +1,28 @@
+fruits = ['morango', 'uva', 'kiwi', 'manga', 'abacaxi']
+
+if 'morango' in fruits:
+    print('morango está na lista')
+
+if 'uva' in fruits:
+    print('uva está na lista')
+
+if 'kiwi' in fruits:
+    print('kiwi está na lista')
+
+if 'manga' in fruits:
+    print('manga está na lista')
+
+
+favorite_fruits = ['morango', 'uva', 'kiwi']
+
+if 'morango' in favorite_fruits:
+    print(f'Você realmente gosta de morango!')
+
+if 'uva' in favorite_fruits:
+    print(f'Você realmente gosta de uva!')
+
+if 'kiwi' in favorite_fruits:
+    print(f'Você realmente gosta de kiwi!')
+
+if 'manga' in favorite_fruits:
+    print(f'Você realmente gosta de morango!')
