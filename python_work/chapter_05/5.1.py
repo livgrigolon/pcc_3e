54b60e47a3f9314c239b425ce325e77dfc134d4f
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-09-06 19:37:06 -0300

    Create 5.1.py

---
+++ python_work/chapter_05/5.1.py
@@ -0,0 +1,47 @@
+# Exemplo
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+
+idade = 18
+print("Is idade == 18? I predict True.")
+print(idade == 18)
+
+idade = 18
+print("Is idade == 18? I predict False.")
+print(idade == '18') # False
+
+role = 'Sofware Dev'
+print("Is role == Sofware Dev? I predict True.")
+print(role == 'Sofware Dev')
+
+role = 'Sofware Dev'
+print("Is role == 18? I predict False.")
+print(role == 'Support')
+
+animal = 'gato'
+print("Is animal == 'gato'? I predict True")
+print(animal == 'gato')
+
+animal = 'cachorro'
+print("Is animal == 'gato'? I predict False")
+print(animal == 'gato')
+
+food = 'hamburguer'
+print("Is food == 'hamburguer'? I predict True")
+print(food == 'hamburguer')
+
+food = 'pizza'
+print("Is food == 'hamburguer'? I predict False")
+print(food == 'hamburguer')
+
+country = 'Italy'
+print("Is country == 'Italy'? I predict True")
+print(country == 'Italy')
+
+country = 'Brazil'
+print("Is country == 'Italy'? I predict False")
+print(country == 'Italy')
