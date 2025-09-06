0b4fd8c5b19c448258907655e456a0a540e6c256
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-09-06 19:37:13 -0300

    Create 5.4.py

---
+++ python_work/chapter_05/5.4.py
@@ -0,0 +1,17 @@
+# Executa o bloco if
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('Você acabou de ganhar 5 pontos por abrir fogo contra um alienígena!')
+else:
+    alien_color != 'green'
+    print('Você acabou de ganhar 10 pontos!')
+
+# Executa o bloco else
+# alien_color = 'red'
+
+# if alien_color == 'green':
+#     print('Você acabou de ganhar 5 pontos por abrir fogo contra um alienígena!')
+# else:
+#     alien_color != 'green'
+#     print('Você acabou de ganhar 10 pontos!')
