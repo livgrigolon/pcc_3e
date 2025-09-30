57cc3a8eb93b249e2a4533ba9710fc184484cfbe
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-09-29 22:21:45 -0300

    Create 8.5.py

---
+++ python_work/chapter_08/8.5.py
@@ -0,0 +1,6 @@
+def describe_city(city, country = 'Brasil'):
+    print(f'{city} fica no(a) {country}')
+
+describe_city('Rio de Janeiro')
+describe_city('São Paulo')
+describe_city('Roma', 'Itália')
