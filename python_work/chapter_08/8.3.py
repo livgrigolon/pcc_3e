9288a9eb4a90714a940a1edafc783aa48969b105
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-09-29 22:21:40 -0300

    Create 8.3.py

---
+++ python_work/chapter_08/8.3.py
@@ -0,0 +1,8 @@
+def make_shirt(size, text):
+    print(f'A camiseta escolhida é tamanho {size} com a frase "{text}".')
+
+# Argumentos posicionais
+make_shirt('G', 'the last dance')
+
+# Argumentos nomeados
+make_shirt(size="M", text="data is the new oil")
