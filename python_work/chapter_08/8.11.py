006c06e889c2abec3687782b3725aa5c0efeb1f9
Lívia Grigolon Homem de Mello <liviaghmello@gmail.com>
2025-10-04 13:11:14 -0300

    Create 8.11.py

---
+++ python_work/chapter_08/8.11.py
@@ -0,0 +1,15 @@
+messages = ['Oi!', 'Não posso falar agora.', 'Estou ocupada!']
+
+# Lista que vai receber as mensagens enviadas
+sent_messages = []
+
+def send_messages(originais, enviadas):
+    while originais:  # enquanto a lista original não estiver vazia
+        message = originais.pop(0)  # pega a primeira mensagem
+        print("Enviando:", message)
+        enviadas.append(message)  # adiciona na lista de enviadas
+
+send_messages(messages[:], sent_messages) # [:] Cópia da lista
+
+print("Mensagens originais (depois do envio):", messages) # Não precisar printar com f-string
+print("Mensagens enviadas:", sent_messages)
